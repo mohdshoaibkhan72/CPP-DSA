d76551e96b1b993680bdaffa5d2bd3fe9d8dab1e
mohammad shoaib khan <mohdshoaibkhan7211@gmail.com>
2024-01-01 23:57:25 +0530

    ptternprintiing

---
+++ patter1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// basic pattern printing in cpp
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            cout << "*";
+        }
+        cout << "" << endl;
+    }
+
+} // namespace std;
