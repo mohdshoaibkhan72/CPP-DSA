05117c0261b858281352244f3028fdbe26428a94
mohammad shoaib khan <mohdshoaibkhan7211@gmail.com>
2024-01-01 23:31:35 +0530

    first commit

---
+++ 01-JAN-2024/condition.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a = 18;
+
+    if (19 >= a)
+    {
+        /* code */
+        cout << "Eligible for vote " << endl;
+    }
+    else
+    {
+        cout << " not aligble ";
+    }
+
+} // namespace std;
---
+++ 01-JAN-2024/heloo.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "this is a my first program going to lern Dsa   ";
+}
